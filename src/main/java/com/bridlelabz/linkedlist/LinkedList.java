a8345f02a43d57ff35768402652c875c17b348f2
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-01-25 12:01:23 +0530

    ADEED:A Simple Linked List

--- src/main/java/com/bridlelabz/linkedlist/LinkedList.java
+++ src/main/java/com/bridlelabz/linkedlist/LinkedList.java
@@ -1,8 +1,25 @@
 package com.bridlelabz.linkedlist;
 
 public class LinkedList {
+    Node head;
+    static class Node {
+        int value;
+        Node next;
+        Node(int data) {
+            value = data;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked list");
-
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = new Node(56);
+        Node second = new Node(30);
+        Node third = new Node(70);
+        linkedList.head.next = second;
+        second.next = third;
+        System.out.print("LinkedList: ");
+        while (linkedList.head != null) {
+            System.out.print(linkedList.head.value + " ");
+            linkedList.head = linkedList.head.next;
+        }
     }
 }
